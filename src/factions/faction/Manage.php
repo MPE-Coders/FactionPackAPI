0044cfe39ae9a6655615b670cd60cd2604a1bf9c
XackiGiFF <m.girsanoff@gmail.com>
2024-02-03 04:37:10 +0300

    Update. Add Special Skills

---
+++ src/factions/faction/Manage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace XackiGiFF\FactionPackAPI\factions\faction;
+
+class Manage
+{
+
+    public function __construct()
+    {
+    }
+}
---
+++ src/factions/faction/SpecialSkils.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace XackiGiFF\FactionPackAPI\factions\faction;
+
+class SpecialSkils
+{
+
+}
